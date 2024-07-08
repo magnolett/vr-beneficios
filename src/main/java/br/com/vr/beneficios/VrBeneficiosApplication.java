e92fcaf0334c271273228b882f8a5c87eb3556bd
Marcos Agnoletto <msilva.rcr@pedepronto.com.br>
2024-07-08 02:53:07 -0300

    First commit

---
+++ src/main/java/br/com/vr/beneficios/VrBeneficiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.vr.beneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrBeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrBeneficiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vr/beneficios/VrBeneficiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vr.beneficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrBeneficiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
